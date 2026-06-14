96329a861b445add427ea683522fd9e33e369651
Nishank Gupta <gnishank8076@gmail.com>
2026-06-14 18:41:34 +0530

    Javascirpt Exercise

---
+++ javascript/pratice.js
@@ -0,0 +1,76 @@
+// let myArr = [
+//     {
+//         name: "John",
+//         age: 30
+//     },
+//     {
+//         name: "Jane",
+//         age: 25
+//     }
+// ];
+
+// myArr.forEach((myarr)=>{
+//     console.log(myarr.age);
+// })
+
+let myArr =[1,2,3,4,5];
+
+// for(let i=0; i<myArr.length; i++){
+//     if(myArr[i] === 3){
+//         console.log("Found 3, skipping...");
+//         continue;
+//     }
+//     console.log(myArr[i]);
+// }
+
+// let shopingcart ={
+//     name: "John",
+//     items: ["apple", "banana", "orange"],
+//     total: 10       
+// }
+
+// for(const element of shopingcart.name){
+    
+//     console.log(element );
+// }
+// function PrimeNumber(num){
+//     if(num <= 1){
+//         return false;
+//     }
+//     for(let i=2; i<=Math.sqrt(num); i++){
+//         if(num % i === 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(PrimeNumber(4));
+
+// not working code 
+// (num)=>{
+//     if(num <= 1){
+//         return false;
+//     }
+//     for(let i=2; i<=Math.sqrt(num); i++){
+//         if(num % i === 0){
+//             console.log("Not a prime number");
+//             return false;
+//         }
+//     }
+//     console.log("Prime number");
+//     return true;    
+// };(4)
+
+// function palindrome(str){
+//     let cleannedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+//     let reversedStr = cleannedStr.split('').reverse().join('');
+//     return cleannedStr === reversedStr; 
+//     console.log(cleannedStr);
+//     console.log(reversedStr);   
+
+// }
+
+// console.log(palindrome("A man, a plan, a canal, Panama"));  
+
+
